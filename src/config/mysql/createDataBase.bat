820b84058d9eccc323644ac0004a8e353e9f34bd
didier <didier-defrance@wanadoo.fr>
2018-09-05 13:52:23 +0200

    avec mysql

---
+++ src/config/mysql/createDataBase.bat
@@ -0,0 +1,6 @@
+REM open with text editor to open/edit in eclipse
+REM open with system editor to launch
+cd /d "%~dp0"
+set MYSQL_HOME=C:\Program Files\MariaDB 10.3
+"%MYSQL_HOME%/bin/mysql" -u root -p < createDataBase.sql
+REM pause

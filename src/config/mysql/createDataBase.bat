32f37f262c47b914b476d5678285c9d7d29e30a0
didier <didier-defrance@wanadoo.fr>
2018-09-05 13:58:35 +0200

    better sql script

--- src/config/mysql/createDataBase.bat
+++ src/config/mysql/createDataBase.bat
@@ -3,4 +3,4 @@ REM open with system editor to launch
 cd /d "%~dp0"
 set MYSQL_HOME=C:\Program Files\MariaDB 10.3
 "%MYSQL_HOME%/bin/mysql" -u root -p < createDataBase.sql
-REM pause
+pause
--- src/config/mysql/createDataBase.sql
+++ src/config/mysql/createDataBase.sql
@@ -1,1 +1,5 @@
 create database if not exists compteDB;
+use compteDB;
+show tables;
+# describe compte;
+# select * from compte;

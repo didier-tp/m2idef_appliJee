f6d6bc9383e04414dcd424a651d9255f36ee9d8e
didier <didier-defrance@wanadoo.fr>
2018-09-20 15:15:17 +0200

    jwtutil

--- appliJee-web/src/main/java/fr/m2i/formation/appliJee/web/rest/CompteRest.java
+++ appliJee-web/src/main/java/fr/m2i/formation/appliJee/web/rest/CompteRest.java
@@ -20,51 +20,52 @@ import fr.m2i.formation.appliJee.service.IServiceCompte;
  */
 
 @Path("service/compte")
-@Produces("application/json") //en retour Java --> JSON
-@Consumes("application/json") //en entree (POST) JSON --> Java
+@Produces("application/json") // en retour Java --> JSON
+@Consumes("application/json") // en entree (POST) JSON --> Java
 public class CompteRest {
-	
-	//le ancien @EJB n'est pas interprété ici dans la techno récente JAX-RS
-	@Inject //@Inject est une annotation un peu plus récente de CDI/JEE6
-	private IServiceCompte serviceCompte; //ejb vers lequel faire des appels
-	
+
+	// le ancien @EJB n'est pas interprété ici dans la techno récente JAX-RS
+	@Inject // @Inject est une annotation un peu plus récente de CDI/JEE6
+	private IServiceCompte serviceCompte; // ejb vers lequel faire des appels
+
 	@GET
 	@Path("/{num}")
-	//@JwtTokenNeeded
+	@JwtTokenNeeded
 	// URL= http://localhost:8080/appliJee-web/rest/service/compte/2
 	public Compte getCompteByNum(@PathParam("num") Long num) {
-		//v1 sans lien avec EJB
-		//return new Compte(num , "compte " + num , 50.0);
-		//v2 avec EJB:
-		//return serviceCompte.rechercherCompteParNumero(num);
+		// v1 sans lien avec EJB
+		// return new Compte(num , "compte " + num , 50.0);
+		// v2 avec EJB:
+		// return serviceCompte.rechercherCompteParNumero(num);
 		return serviceCompte.rechercherCompteAvecOperationsParNumero(num);
 	}
-	
-	//a tester avec PostMan , avec Content-Type = application/json dans Headers
-	//et Body en mode "raw"
+
+	// a tester avec PostMan , avec Content-Type = application/json dans Headers
+	// et Body en mode "raw"
 	@POST
 	@Path("")
-	// URL= http://localhost:8080/appliJee-web/rest/service/compte appelé en method=POST
-	// avec { "numero": null ou 3 , "label": "compte xy" , "solde" : 50.0 } 
+	// URL= http://localhost:8080/appliJee-web/rest/service/compte appelé en
+	// method=POST
+	// avec { "numero": null ou 3 , "label": "compte xy" , "solde" : 50.0 }
 	// dans le corps/body invisible de la requete en mode "raw"
-	//et au format Content-Type=application/json
+	// et au format Content-Type=application/json
 	public Compte postCompte(Compte compte) {
 		serviceCompte.saveOrUpdateCompte(compte);
-		return compte;//en retour , copie du compte sauvegardé avec clef primaire quelquefois auto_incr
+		return compte;// en retour , copie du compte sauvegardé avec clef primaire quelquefois
+						// auto_incr
 	}
-	
-	
+
 	@GET
 	@Path("")
 	// URL= http://localhost:8080/appliJee-web/rest/service/compte?numMax=2
 	public List<Compte> getComptesByNumMax(@QueryParam("numMax") Long numMax) {
-		//v1 sans lien avec EJB , v2 avec EJB
+		// v1 sans lien avec EJB , v2 avec EJB
 		List<Compte> listeComptes = new ArrayList<Compte>();
-		  //if(numMax>=1) listeComptes.add(new Compte(1L , "compte 1"  , 50.0));
-		  //if(numMax>=2) listeComptes.add(new Compte(2L , "compte 2"  , 80.0));
-		 for(long i=1;i<=numMax;i++)
-			 listeComptes.add(serviceCompte.rechercherCompteAvecOperationsParNumero(i));
-		 //System.out.println("listeComptes:"+listeComptes);
+		// if(numMax>=1) listeComptes.add(new Compte(1L , "compte 1" , 50.0));
+		// if(numMax>=2) listeComptes.add(new Compte(2L , "compte 2" , 80.0));
+		for (long i = 1; i <= numMax; i++)
+			listeComptes.add(serviceCompte.rechercherCompteAvecOperationsParNumero(i));
+		// System.out.println("listeComptes:"+listeComptes);
 		return listeComptes;
 	}
 
--- appliJee-web/src/main/java/fr/m2i/formation/appliJee/web/rest/JwtTokenNeededFilter.java
+++ appliJee-web/src/main/java/fr/m2i/formation/appliJee/web/rest/JwtTokenNeededFilter.java
@@ -1,49 +1,62 @@
 package fr.m2i.formation.appliJee.web.rest;
 
 import java.io.IOException;
-import java.security.Key;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.annotation.Priority;
-import javax.crypto.KeyGenerator;
-import javax.inject.Inject;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+
+import org.mycontrib.generic.security.jwt.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 @JwtTokenNeeded
 @Priority(Priorities.AUTHENTICATION)
 public class JwtTokenNeededFilter implements ContainerRequestFilter {
-	
- /*
-    @Inject
-    private KeyGenerator keyGenerator;
-    */
- 
-    @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-        /*
-        // Get the HTTP Authorization header from the request
-        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
- 
-        // Extract the token from the HTTP Authorization header
-        String token = authorizationHeader.substring("Bearer".length()).trim();
-       
-        try {
- 
-            // Validate the token
-            Key key = keyGenerator.generateKey();
-            Jwts.parser().setSigningKey(key).parseClaimsJws(token);
-            logger.info("#### valid token : " + token);
- 
-        } catch (Exception e) {
-            logger.severe("#### invalid token : " + token);
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
-        }
-        */
-    	System.out.println("JWTTokenNeededFilter.filter() was called");
-    }
-}
 
+	private static final Logger logger = LoggerFactory.getLogger(JwtTokenNeededFilter.class);
+
+	/*
+	 * @Inject private KeyGenerator keyGenerator;
+	 */
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		/*
+		 * // Get the HTTP Authorization header from the request String
+		 * authorizationHeader =
+		 * requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+		 * 
+		 * // Extract the token from the HTTP Authorization header String token =
+		 * authorizationHeader.substring("Bearer".length()).trim();
+		 * 
+		 * try {
+		 * 
+		 * // Validate the token Key key = keyGenerator.generateKey();
+		 * Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+		 * logger.info("#### valid token : " + token);
+		 * 
+		 * } catch (Exception e) { logger.severe("#### invalid token : " + token);
+		 * requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build(
+		 * )); }
+		 */
+
+		// Test temporaire:
+		System.out.println("JWTTokenNeededFilter.filter() was called");
+		long jwtExpirationInMs = 60 * 15 * 1000; // = 900000ms pour 15minutes
+		String username = "user1";
+		String secretKey = "mySecretKey";
+		Set<String> roles = new HashSet<String>();
+		roles.add("USER");
+		roles.add("MEMBER");
+		String myJwt = JwtUtil.buildToken(username, jwtExpirationInMs, secretKey, roles);
+		logger.info("myJwt=" + myJwt);
+		logger.info("claims in jwt=" + JwtUtil.extractClaimsFromJWT(myJwt, secretKey).toString());
+
+	}
+}
